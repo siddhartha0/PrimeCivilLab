c46a695f95c87b476480a94a2b4b69b95abfd425
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-29 19:08:54 +0545

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,24 +6,21 @@ module.exports = {
   },
   theme: {
     colors: {
+      brand: "#fa9a00",
+      grey: "#E9E9E9",
       white: "#FFF",
-      bg: "#F8F8F8",
+      "fadish-white": "#cccccc",
+      black: "#333333",
+      "black-3": "#343434",
+      "black-2": "#222222",
+      "black-4": "#444444",
+
       "dark-blue": "#0F82FF",
       "fadish-blue": "#1B9AF7",
       "fade-blue": "#299FE2",
       "fadding-blue": "#79CAFF",
 
-      yellow: "#FEAE1B",
       "milky-green": "#01CD61",
-      black: {
-        1000: "#000000",
-        900: "#444444",
-        800: "#5C5E5C",
-        700: "#545454",
-        600: "#C4CCC4",
-      },
-
-      "fadish-black": "#2b2c30",
     },
     extend: {
       fontFamily: {
