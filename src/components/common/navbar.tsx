e5f9cdea0446708353f5eed9e0b70b95c840ef03
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-31 21:25:03 +0545

    logo fixed

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -37,7 +37,7 @@ export const Navbar = memo(() => {
         <img
           src={logo}
           alt="company logo"
-          className="w-52 cursor-pointer"
+          className="w-52 cursor-pointer -mt-7"
           onClick={() => nav("/")}
         />
       </section>
--- src/components/pages/home/core-two.tsx
+++ src/components/pages/home/core-two.tsx
@@ -1,3 +1,16 @@
+import { Text } from "../../units";
+
 export const BottomCore = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-8 px-32 py-16">
+      <section className="flex flex-col gap-4">
+        <div className="flex place-items-center gap-4">
+          <Text size="body-lg-mid" secondaryFont>
+            Recent New
+          </Text>
+          <div className="h-[.5px] w-[30%] bg-grey" />
+        </div>
+      </section>
+    </div>
+  );
 };
