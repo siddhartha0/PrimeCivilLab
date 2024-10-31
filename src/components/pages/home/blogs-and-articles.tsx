1e6cd44f0081a8daafc05c5d1ad33f28b1024fb3
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-31 23:08:56 +0545

    blogs comps done

--- src/components/pages/home/blogs-and-articles.tsx
+++ src/components/pages/home/blogs-and-articles.tsx
@@ -1,3 +1,63 @@
-export const BlogsAndArticles = () => {
-  return <div></div>;
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Button, Text } from "../../units";
+
+interface propTypes {
+  bgPics: string;
+  title: string;
+  time: Date;
+  content: string;
+}
+
+export const BlogsAndArticles = ({
+  bgPics,
+  title,
+  time,
+  content,
+}: propTypes) => {
+  return (
+    <div
+      className="relative flex flex-col gap-2 w-full group overflow-hidden py-12"
+      style={{
+        background: `url(${bgPics})`,
+      }}
+    >
+      <div className="absolute  inset-0 bg-black-2 opacity-60 "></div>
+
+      {/* Dark overlay and button on hover */}
+      <div className="absolute inset-0 bg-black-2 bg-opacity-0 group-hover:bg-opacity-60 transition duration-300 ease-in-out flex z-30 justify-center items-center">
+        <Button
+          usage="brand"
+          className="opacity-0 group-hover:opacity-100  text-white font-semibold px-4 py-2 rounded transition duration-300 ease-in-out"
+        >
+          Read More
+        </Button>
+      </div>
+
+      {/* Content Section */}
+      <section
+        className="relative  flex flex-col gap-4 p-4 shadow-xl  "
+        id="message"
+      >
+        <Text
+          size="heading-base-mid"
+          className="leading-tight line-clamp-2 "
+          secondaryFont
+          usage="brand"
+        >
+          {title}
+        </Text>
+
+        <div className="flex items-center gap-2 mt-4 ">
+          <Icon icon="carbon:time" color="#fff" />
+          <Text size="body-sm-default" usage="click">
+            {time.toISOString().split("T")[0]}
+          </Text>
+        </div>
+
+        <Text size="body-sm-default" usage="click" className="line-clamp-5">
+          {content}
+        </Text>
+      </section>
+    </div>
+  );
 };
--- src/components/pages/home/core-two.tsx
+++ src/components/pages/home/core-two.tsx
@@ -1,14 +1,64 @@
 import { Text } from "../../units";
+import { BlogsAndArticles } from "./blogs-and-articles";
+import bgone from "../../../assets/images/blog1.jpg";
+import bgtwo from "../../../assets/images/bg-office-black.jpg";
+
+export const BlogsAndArticlesDatas = [
+  {
+    id: 1,
+    title: "The Future of Sustainable Engineering",
+    content:
+      "Explore the latest advancements in sustainable engineering and how our company is committed to eco-friendly solutions in every project.",
+    time: new Date("2024-01-15"),
+    bgpics: bgone,
+  },
+  {
+    id: 2,
+    title: "Innovative Foundation Engineering for High-Rise Buildings",
+    content:
+      "Discover the methods used in foundation engineering to support tall structures, ensuring stability in complex terrains and challenging soil conditions.",
+    time: new Date("2024-03-10"),
+    bgpics: bgtwo,
+  },
+  {
+    id: 3,
+    title: "The Role of Geotechnical Engineering in Urban Development",
+    content:
+      "From skyscrapers to subways, see how geotechnical assessments shape safe and efficient urban expansion.",
+    time: new Date("2024-05-18"),
+    bgpics: bgone,
+  },
+  {
+    id: 4,
+    title: "From Field to Data: Digital Tools in Soil Analysis",
+    content:
+      "An overview of the digital tools that have transformed soil analysis, enhancing precision and data accessibility.",
+    time: new Date("2025-02-23"),
+    bgpics: bgtwo,
+  },
+];
 
 export const BottomCore = () => {
   return (
     <div className="flex flex-col gap-8 px-32 py-16">
-      <section className="flex flex-col gap-4">
-        <div className="flex place-items-center gap-4">
+      <section className="flex flex-col gap-8" id="blogs_and_articles">
+        <div className="flex place-items-center gap-6">
           <Text size="body-lg-mid" secondaryFont>
             Recent New
           </Text>
-          <div className="h-[.5px] w-[30%] bg-grey" />
+          <span className="h-[.5px] w-[40%] bg-grey" />
+        </div>
+        <div className="flex w-[620px] gap-10 overflow-scroll ">
+          {BlogsAndArticlesDatas.map((value) => (
+            <div className="min-w-[280px]">
+              <BlogsAndArticles
+                bgPics={value.bgpics}
+                content={value.content}
+                time={value.time}
+                title={value.title}
+              />
+            </div>
+          ))}
         </div>
       </section>
     </div>
