1f72754a6edf1f7ce0b385db817b21a2e89c9e95
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-11-03 18:30:04 +0545

    animation added

--- src/components/pages/home/core-three.tsx
+++ src/components/pages/home/core-three.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Link } from "react-router-dom";
 import { Button, Text } from "../../units";
 import { motion } from "framer-motion";
@@ -7,14 +9,14 @@ import bg from "../../../assets/images/blog1.jpg";
 export const CoreThree = () => {
   return (
     <div
-      className="flex flex-col  gap-8 px-32 py-16 w-full"
+      className="flex flex-col gap-8 px-32 py-16 w-full"
       id="parent_div"
       style={{
         background: `url(${bg})`,
       }}
     >
       <section
-        className="flex place-items-center gap-7  w-full justify-center"
+        className="flex place-items-center gap-7 w-full justify-center"
         id="project-header-div"
       >
         <span className="h-[.5px] w-[30%] bg-fadish-grey" id="border" />
@@ -25,7 +27,7 @@ export const CoreThree = () => {
       </section>
 
       <section
-        className="flex place-items-center justify-center gap-6  "
+        className="flex place-items-center justify-center gap-6"
         id="project-nav-div"
       >
         <div className="flex flex-col gap-1 group/recent" id="recent-div">
@@ -33,29 +35,29 @@ export const CoreThree = () => {
             Recents
           </Link>
           <motion.div
-            className="h-[2px]  w-full   top-7 bg-brand origin-left scale-x-0 group-hover/recent:scale-x-100 transition-transform duration-300"
+            className="h-[2px] w-full top-7 bg-brand origin-left scale-x-0 group-hover/recent:scale-x-100 transition-transform duration-300"
             id="bottom-animated-border"
           />
         </div>
 
         <div className="flex flex-col gap-1 group/more" id="more-div">
-          <Link to="" id="more-link" className=" relative text-fadish-grey">
+          <Link to="" id="more-link" className="relative text-fadish-grey">
             More
           </Link>
           <motion.div
-            className="h-[2px]  w-full   top-7 bg-brand origin-left scale-x-0 group-hover/more:scale-x-100 transition-transform duration-300"
+            className="h-[2px] w-full top-7 bg-brand origin-left scale-x-0 group-hover/more:scale-x-100 transition-transform duration-300"
             id="bottom-animated-border"
           />
         </div>
       </section>
 
-      <section id="project-gallery-view" className="grid grid-cols-3 gap-8 ">
+      <section id="project-gallery-view" className="grid grid-cols-3 gap-8">
         {Projects.map((project, i) => {
           return (
-            <section
+            <div
               key={project.title + i}
-              className={`flex flex-col group  min-h-96 relative ${
-                i == 1 ? "row-span-2" : ""
+              className={`flex flex-col group min-h-96 relative ${
+                i === 1 ? "row-span-2" : ""
               }`}
               id="projects-parent"
               style={{
@@ -66,32 +68,38 @@ export const CoreThree = () => {
               }}
             >
               <div
-                className="absolute flex-col inset-0 bg-black-2 bg-opacity-0 group-hover:bg-opacity-60 transition duration-300 ease-in-out flex z-30 justify-center"
+                className="absolute flex-col inset-0 bg-black-2 bg-opacity-0 group-hover:bg-opacity-60 flex z-30 justify-center transition-all duration-300"
                 id="hidden-content-div"
               >
-                <Text
-                  className="opacity-0 group-hover:opacity-100  text-white font-semibold px-4 py-2 rounded transition duration-300 ease-in-out"
-                  size="body-base-default"
-                  secondaryFont
-                  id="category"
-                >
-                  {project.category}
-                </Text>
-                <Text
-                  className="opacity-0 group-hover:opacity-100  text-white font-semibold px-4 py-2 rounded transition duration-300 ease-in-out"
-                  id="title"
-                >
-                  {project.title}
-                </Text>
-                <Button
-                  usage="brand"
-                  className="opacity-0 group-hover:opacity-100 mt-7  text-white font-semibold px-4 py-2 rounded transition duration-300 ease-in-out"
-                  id="button"
-                >
-                  View
-                </Button>
+                <div className="transform translate-y-4 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-300 delay-100">
+                  <Text
+                    className="text-white font-semibold px-4 py-2 rounded"
+                    size="body-base-default"
+                    secondaryFont
+                    id="category"
+                  >
+                    {project.category}
+                  </Text>
+                </div>
+                <div className="transform translate-y-4 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-300 delay-200">
+                  <Text
+                    className="text-white font-semibold px-4 py-2 rounded"
+                    id="title"
+                  >
+                    {project.title}
+                  </Text>
+                </div>
+                <div className="transform translate-y-4 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-300 delay-300 px-4">
+                  <Button
+                    usage="brand"
+                    className="mt-7 text-white font-semibold py-2 rounded"
+                    id="button"
+                  >
+                    View
+                  </Button>
+                </div>
               </div>
-            </section>
+            </div>
           );
         })}
       </section>
