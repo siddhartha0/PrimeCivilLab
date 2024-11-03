41bd8a3180d4d79198315a94558cf33f0cc03333
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-11-03 13:31:43 +0545

    pages refactored

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { Home } from "./pages/home/home";
+import { Home } from "./pages/home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
