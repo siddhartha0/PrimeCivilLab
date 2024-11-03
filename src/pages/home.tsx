22fc7eeb6aaf19ac77e689f8bf8d31ce71024d44
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-11-03 13:31:14 +0545

    refactored init

---
+++ src/pages/home.tsx
@@ -0,0 +1,21 @@
+import { LandingPageLayout } from "../components/common/landingpagelayout";
+import { Banner } from "../components/pages/home/banner";
+import { Core } from "../components/pages/home/core";
+import { CoreThree } from "../components/pages/home/core-three";
+import { CoreTwo } from "../components/pages/home/core-two";
+
+export const Home = () => {
+  return (
+    <LandingPageLayout>
+      <div className="flex flex-col ">
+        <Banner />
+        <div className="pt-72">
+          <Core />
+        </div>
+        <CoreTwo />
+
+        <CoreThree />
+      </div>
+    </LandingPageLayout>
+  );
+};
