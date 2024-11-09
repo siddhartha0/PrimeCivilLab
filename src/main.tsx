93c3e854fdf4ac6aa72e9aed93e597f959d165d6
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-11-09 20:00:45 +0545

    view init

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Projects } from "./pages/projects/projects";
+import ProjectView from "./pages/projects/project-view";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Projects />} path="projects" />
+        <Route element={<ProjectView />} path="/projects/project-view" />
       </Routes>
     </BrowserRouter>
   </StrictMode>
